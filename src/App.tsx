8f34239a5c9859c010b58ad4c17d1fd69d507125
caiopawilak@gmail.com <39964779+CaioPawilak@users.noreply.github.com>
2021-04-02 23:33:01 -0300

    Conclusão do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 
-import React from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal'
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { useState } from 'react';
 
+Modal.setAppElement('#root')
 
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransaction() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransaction() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransaction} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransaction}
+      />
+
       <GlobalStyle />
     </>
   );
